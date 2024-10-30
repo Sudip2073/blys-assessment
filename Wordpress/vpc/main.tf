db34b6e06650765be011c31e426660ef01eb924b
Sudip <shresthasudeep2073@gmail.com>
2024-10-30 17:56:45 +0545

    Internet gateway added

--- Wordpress/vpc/main.tf
+++ Wordpress/vpc/main.tf
@@ -16,6 +16,29 @@ resource "aws_subnet" "public" {
   }
 }
 
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+}
+
+resource "aws_route_table" "public_route_table" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "public-route-table"
+  }
+}
+
+resource "aws_route_table_association" "public_subnet_assoc" {
+  count          = 2
+  subnet_id      = aws_subnet.public[count.index].id
+  route_table_id = aws_route_table.public_route_table.id
+}
+
 resource "aws_security_group" "alb_sg" {
   name        = "alb-security-group"
   description = "Allow inbound traffic for ALB"
